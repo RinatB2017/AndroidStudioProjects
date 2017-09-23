c9ecb4d4b16b7c3f3de647ea724fa5e47c94e357
RinatB2017 <tux4096@gmail.com>
2017-09-24 02:59:56 +0300

    update

--- GitHub/BinauralBeatsExample_android/app/src/main/java/com/guidedmeditationtreks/binaural/MainActivity.java
+++ GitHub/BinauralBeatsExample_android/app/src/main/java/com/guidedmeditationtreks/binaural/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -114,6 +116,19 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		// займем весь экран
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION           // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN                // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+		//---
 
 		setContentView(R.layout.main_activity);
 		initializeView();
