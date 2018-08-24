901c9a1a994c4e48b4e5aee9df350cf030eff07d
RinatB2017 <tux4096@gmail.com>
2018-08-24 21:44:00 +0300

    update

--- Template/app/src/main/java/com/boss/template/MainActivity.java
+++ Template/app/src/main/java/com/boss/template/MainActivity.java
@@ -1,7 +1,10 @@
 package com.boss.template;
 
+import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,8 +15,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
     static final String LOG_TAG = "States";
+
+    private ProgressDialog scanProgressDialog;
+    Handler handler;
+
+    final Random random = new Random();
 
     TextView tv_log;
 
@@ -117,6 +127,62 @@ public class MainActivity extends AppCompatActivity{
     //---------------------------------------------------------------------------------------------
     public void click(View view) {
         logging("test");
+
+        // https://javadevblog.com/dialogi-v-android-primer-raboty-s-progressdialog.html
+        // http://developer.alexanderklimov.ru/android/java/random.php
+
+        int minValue = 0;
+        int maxValue = 100;
+
+        scanProgressDialog = new ProgressDialog(MainActivity.this);
+        scanProgressDialog.setCancelable(false);
+        scanProgressDialog.setTitle("Scanning: " + minValue + " to " + maxValue);
+        scanProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        scanProgressDialog.incrementProgressBy(1);
+        scanProgressDialog.setProgress(minValue);
+        scanProgressDialog.setMax(maxValue);
+        scanProgressDialog.show();
+
+        handler = new Handler() {
+            public void handleMessage(Message msg) {
+                // и обновляем идикатор, пока шкала не заполнится
+                if (scanProgressDialog.getProgress() < scanProgressDialog.getMax()) {
+                    //scanProgressDialog.setProgress(msg.what);
+                    // обновляем индикаторы на 3 пункта за 1 секунду (1000 милисекунд)
+                    scanProgressDialog.incrementProgressBy(1);
+                    handler.sendEmptyMessageDelayed(0, 1000);
+                } else {
+                    // когда шкала заполнилась, диалог пропадает
+                    scanProgressDialog.dismiss();
+                }
+            }
+        };
+        // имитируем подключение к удаленному серверу
+        // (ожидаем 1 секунду перед стартом обновления индикатора)
+        handler.sendEmptyMessageDelayed(0, 1000);
+
+        /*
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 1; i <= 10; i++) {
+                    // долгий процесс
+                    try {
+                        int n = random.nextInt(10) * 1000;
+                        Thread.sleep(1000 + n);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    handler.sendEmptyMessage(i);
+                    // пишем лог
+                    Log.d(LOG_TAG, "i = " + i);
+                }
+            }
+        });
+        t.start();
+        */
+
+        logging("the end!");
     }
 
     //---------------------------------------------------------------------------------------------
