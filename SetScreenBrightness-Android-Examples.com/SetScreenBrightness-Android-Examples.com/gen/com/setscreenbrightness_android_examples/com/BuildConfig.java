448c8144abca7a3b1576f524329295f153b53668
RinatB2017 <tux4096@gmail.com>
2018-08-25 22:31:27 +0300

    update

--- SetScreenBrightness-Android-Examples.com/SetScreenBrightness-Android-Examples.com/gen/com/setscreenbrightness_android_examples/com/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.setscreenbrightness_android_examples.com;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
