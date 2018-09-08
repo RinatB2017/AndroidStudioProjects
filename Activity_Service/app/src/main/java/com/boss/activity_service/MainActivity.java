abd73f55d586c16893d5020bd0388d2ea17b1042
RinatB2017 <tux4096@gmail.com>
2018-09-09 00:38:12 +0300

    update

---
+++ Activity_Service/app/src/main/java/com/boss/activity_service/MainActivity.java
@@ -0,0 +1,103 @@
+package com.boss.activity_service;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+// http://www.pvsm.ru/razrabotka-pod-android/18682
+
+public class MainActivity extends Activity {
+    public static final String TAG = "TestService";
+
+    TestServiceConnection testServConn;
+    TextView testTxt;
+
+    final Messenger messenger = new Messenger(new IncomingHandler());
+    Messenger toServiceMessenger;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        testTxt = (TextView)findViewById(R.id.test_txt);
+
+        bindService(new Intent(this, TestService.class),
+                (testServConn = new TestServiceConnection()),
+                Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+
+        unbindService(testServConn);
+    }
+
+    public void countIncrClick(View button){
+        Message msg = Message.obtain(null, TestService.COUNT_PLUS);
+        msg.replyTo = messenger;
+        try {
+            toServiceMessenger.send(msg);
+        }
+        catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void countDecrClick(View button){
+        Message msg = Message.obtain(null, TestService.COUNT_MINUS);
+        msg.replyTo = messenger;
+        try {
+            toServiceMessenger.send(msg);
+        }
+        catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg){
+            switch (msg.what) {
+                case TestService.GET_COUNT:
+                    Log.d(TAG, "(activity)...get count");
+                    testTxt.setText(""+msg.arg1);
+
+                    break;
+            }
+        }
+    }
+
+    private class TestServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            toServiceMessenger = new Messenger(service);
+            //отправляем начальное значение счетчика
+            Message msg = Message.obtain(null, TestService.SET_COUNT);
+            msg.replyTo = messenger;
+            msg.arg1 = 0; //наш счетчик
+            try {
+                toServiceMessenger.send(msg);
+            }
+            catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {	}
+    }
+}
---
+++ Activity_Service/app/src/main/java/com/boss/activity_service/TestService.java
@@ -0,0 +1,89 @@
+package com.boss.activity_service;
+
+import android.app.Service;
+import android.content.*;
+import android.os.*;
+import android.os.Process;
+import android.util.Log;
+
+public class TestService extends Service {
+    public static final int COUNT_PLUS = 1;
+    public static final int COUNT_MINUS = 2;
+	public static final int SET_COUNT = 0;
+	public static final int GET_COUNT = 3;
+	
+	int count = 0;
+	
+	IncomingHandler inHandler;
+	
+	Messenger messanger;
+	Messenger toActivityMessenger;
+	
+	@Override
+	public void onCreate(){
+		super.onCreate();
+		
+		HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+	    thread.start();
+
+	    inHandler = new IncomingHandler(thread.getLooper());
+	    messanger = new Messenger(inHandler);
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return messanger.getBinder();
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		return START_STICKY;
+	}
+	
+	//обработчик сообщений активити
+	private class IncomingHandler extends Handler {
+		public IncomingHandler(Looper looper){
+			super(looper);
+		}
+		
+		@Override
+		public void handleMessage(Message msg){
+			//super.handleMessage(msg);
+			
+			toActivityMessenger = msg.replyTo;
+			
+			switch (msg.what) {
+			case SET_COUNT:
+				count = msg.arg1; 
+				
+				Log.i(MainActivity.TAG, "(service)...set count");
+				break;
+			case COUNT_PLUS:
+				count++;
+				
+				Log.i(MainActivity.TAG, "(service)...count plus");
+				break;
+				
+			case COUNT_MINUS:
+				
+				Log.i(MainActivity.TAG, "(service)...count minus");
+				count--;
+				
+				break;
+			}
+			
+			//отправляем значение счетчика в активити
+			Message outMsg = Message.obtain(inHandler, GET_COUNT);
+			outMsg.arg1 = count;
+			outMsg.replyTo = messanger;
+			
+			try {
+				if( toActivityMessenger != null )
+				    toActivityMessenger.send(outMsg);
+			} 
+			catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
