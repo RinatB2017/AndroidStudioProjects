186f4326aeada3d80f6a1224f53b483bf9eb0bb2
RinatB2017 <tux4096@gmail.com>
2018-09-27 11:57:06 +0300

    update

--- Test_NFC/app/src/main/java/com/example/user/test_nfc/MainActivity.java
+++ Test_NFC/app/src/main/java/com/example/user/test_nfc/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends LogActivity {
     private String[][] nfctechfilter = new String[][]{new String[]{NfcA.class.getName()}};
     private PendingIntent nfcintent;
 
+    //private NfcAdapter nfcAdapter;
+
     //---------------------------------------------------------------------------------------------
     protected void requestPermission(String permissionType, int requestCode) {
         int permission = ContextCompat.checkSelfPermission(this,
@@ -45,6 +47,23 @@ public class MainActivity extends LogActivity {
         nfcintent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         //---
 
+        //---
+        /*
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(this,
+                    "NFC NOT supported on this devices!",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        } else if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(this,
+                    "NFC NOT Enabled!",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }
+        */
+        //---
+
         //send_log("MainActivity: onCreate()");
     }
 
@@ -55,7 +74,7 @@ public class MainActivity extends LogActivity {
         tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         send_log("Tag detected!");
 
-        if(tag != null) {
+        if (tag != null) {
             byte[] id = tag.getId();
             send_log(SharedUtils.Byte2Hex(id));
         }
@@ -76,6 +95,51 @@ public class MainActivity extends LogActivity {
             nfc.enableForegroundDispatch(this, nfcintent, null, nfctechfilter);
         }
     }
+
+    //---------------------------------------------------------------------------------------------
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+            Toast.makeText(this,
+                    "onResume() - ACTION_TAG_DISCOVERED",
+                    Toast.LENGTH_SHORT).show();
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if (tag == null) {
+                send_log("tag == null");
+            } else {
+                String tagInfo = tag.toString() + "\n";
+
+                tagInfo += "\nTag Id: \n";
+                byte[] tagId = tag.getId();
+                tagInfo += "length = " + tagId.length + "\n";
+                for (int i = 0; i < tagId.length; i++) {
+                    tagInfo += Integer.toHexString(tagId[i] & 0xFF) + " ";
+                }
+                tagInfo += "\n";
+
+                String[] techList = tag.getTechList();
+                tagInfo += "\nTech List\n";
+                tagInfo += "length = " + techList.length + "\n";
+                for (int i = 0; i < techList.length; i++) {
+                    tagInfo += techList[i] + "\n ";
+                }
+
+                send_log(tagInfo);
+            }
+        } else {
+            Toast.makeText(this,
+                    "onResume() : " + action,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    */
 
     //---------------------------------------------------------------------------------------------
     @Override
