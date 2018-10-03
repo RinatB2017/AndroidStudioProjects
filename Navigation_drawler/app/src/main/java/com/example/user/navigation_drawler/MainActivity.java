44be2cc9e4416e5e363560b76ffdb5a622b3c380
RinatB2017 <tux4096@gmail.com>
2018-10-03 18:45:09 +0300

    update

--- Navigation_drawler/app/src/main/java/com/example/user/navigation_drawler/MainActivity.java
+++ Navigation_drawler/app/src/main/java/com/example/user/navigation_drawler/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.user.navigation_drawler;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,25 +18,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    static final String LOG_TAG = "States";
+
+    TextView tv_log;
+    Handler h_print;
+
+    private DrawerLayout mDrawer;
+
+    //---------------------------------------------------------------------------------------------
+    public void send_log(String text) {
+        if (text == null) {
+            return;
+        }
+        Message msg = new Message();
+        msg.obj = text;
+        h_print.sendMessage(msg);
+    }
+
+    //---------------------------------------------------------------------------------------------
+    void init_log() {
+        tv_log = (TextView) findViewById(R.id.logView);
+        tv_log.setTextColor(Color.BLACK);
+
+        h_print = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String text = (String) msg.obj;
+                Log.i(LOG_TAG, text);
+                tv_log.append(text + "\n");
+            }
+        };
+    }
+
+    //---------------------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init_log();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,8 +83,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 
+    //---------------------------------------------------------------------------------------------
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -52,6 +98,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    //---------------------------------------------------------------------------------------------
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -59,6 +106,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    //---------------------------------------------------------------------------------------------
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -74,28 +122,44 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    //---------------------------------------------------------------------------------------------
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (id) {
+            case R.id.nav_camera:
+                send_log("nav_camera");
+                break;
 
-        } else if (id == R.id.nav_slideshow) {
+            case R.id.nav_gallery:
+                send_log("nav_gallery");
+                break;
 
-        } else if (id == R.id.nav_manage) {
+            case R.id.nav_slideshow:
+                send_log("nav_slideshow");
+                break;
 
-        } else if (id == R.id.nav_share) {
+            case R.id.nav_manage:
+                send_log("nav_manage");
+                break;
 
-        } else if (id == R.id.nav_send) {
+            case R.id.nav_share:
+                send_log("nav_share");
+                break;
 
+            case R.id.nav_send:
+                send_log("nav_send");
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    //---------------------------------------------------------------------------------------------
+
 }
