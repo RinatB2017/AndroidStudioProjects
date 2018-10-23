8552eff6c3f0c4873f4d57238ba59b1c20fd7562
RinatB2017 <tux4096@gmail.com>
2018-10-23 16:59:03 +0300

    update

---
+++ _For_Testing/app/src/main/java/com/boss/for_testing/ServiceReceiver.java
@@ -0,0 +1,23 @@
+package com.boss.for_testing;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class ServiceReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        TelephonyManager telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        telephony.listen(new PhoneStateListener(){
+            @Override
+            public void onCallStateChanged(int state, String incomingNumber) {
+                super.onCallStateChanged(state, incomingNumber);
+                Log.i("States", "incomingNumber : " + incomingNumber);
+            }
+        },PhoneStateListener.LISTEN_CALL_STATE);
+    }
+}
