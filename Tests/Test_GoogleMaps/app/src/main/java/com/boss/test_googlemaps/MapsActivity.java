c25676a155a4177f2089fe1bd977d0fc2abd9f56
RinatB2017 <tux4096@gmail.com>
2018-12-08 11:53:58 +0300

    update

--- Tests/Test_GoogleMaps/app/src/main/java/com/boss/test_googlemaps/MapsActivity.java
+++ Tests/Test_GoogleMaps/app/src/main/java/com/boss/test_googlemaps/MapsActivity.java
@@ -1,19 +1,22 @@
 package com.boss.test_googlemaps;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -45,6 +48,10 @@ import javax.xml.parsers.ParserConfigurationException;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager mLocationManager;
+
+    public static final int LOCATION_UPDATE_MIN_DISTANCE = 10;
+    public static final int LOCATION_UPDATE_MIN_TIME = 5000;
 
     private List<LatLng> places = new ArrayList<>();
     private String mapsApiKey;
@@ -52,6 +59,89 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     final int DEFAULT_ZOOM = 15;
     String path;
     String filename;
+
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            drawMarker(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            if (ActivityCompat.checkSelfPermission(getParent(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(getParent(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            drawMarker(mLocationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            Log.i("States", "onProviderDisabled");
+        }
+    };
+
+
+    private void initMap() {
+        int googlePlayStatus = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (googlePlayStatus != ConnectionResult.SUCCESS) {
+            GooglePlayServicesUtil.getErrorDialog(googlePlayStatus, this, -1).show();
+            finish();
+        } else {
+            if (mMap != null) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                mMap.getUiSettings().setAllGesturesEnabled(true);
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+        boolean isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!(isGPSEnabled || isNetworkEnabled))
+            return;
+        else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            if (isNetworkEnabled) {
+                mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                        LOCATION_UPDATE_MIN_TIME,
+                        LOCATION_UPDATE_MIN_DISTANCE,
+                        mLocationListener);
+            }
+
+            if (isGPSEnabled) {
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        LOCATION_UPDATE_MIN_TIME,
+                        LOCATION_UPDATE_MIN_DISTANCE,
+                        mLocationListener);
+            }
+        }
+    }
+
+    private void drawMarker(Location location) {
+        if (mMap != null) {
+            mMap.clear();
+            LatLng gps = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions()
+                    .position(gps)
+                    .title("Current position"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps, 12));
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +172,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         }
+        //---
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        initMap();
+        getCurrentLocation();
         //---
     }
 
@@ -123,6 +218,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                 float dist = markerLocation1.distanceTo(markerLocation2);
                 Log.i("States", "dist " + dist);
+                break;
+
+            case R.id.action_test2:
+                LatLng marker = new LatLng(50.06169631, 19.93919566);
+                Marker m_test = mMap.addMarker(new MarkerOptions().position(marker).title("Lat: " + marker.latitude + "long: " + marker.longitude));
+                CameraUpdate update = CameraUpdateFactory.newLatLngZoom(marker, DEFAULT_ZOOM);
+                m_test.showInfoWindow();
+                mMap.moveCamera(update);
+                break;
+
+            case R.id.action_test3:
+                getCurrentLocation();
                 break;
 
             default:
