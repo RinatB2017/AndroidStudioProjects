34043e43ef48174501b9f7dc6c38210e7cdf9a1d
RinatB2017 <tux4096@gmail.com>
2019-03-22 00:28:42 +0300

    update

--- _For_Testing/app/src/main/java/com/boss/for_testing/ColorPicker.java
+++ _For_Testing/app/src/main/java/com/boss/for_testing/ColorPicker.java
@@ -5,11 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.SweepGradient;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -20,12 +22,16 @@ public class ColorPicker extends AppCompatImageView
     Canvas c_bitmap;
     Bitmap bitmap;
 
-    int width = 600;
+    int width  = 600;
     int height = 600;
-    int sw = 150;
+    int sw = width / 4;
     int center_x = width / 2;
     int center_y = height / 2;
     int radius = width / 2;
+
+    int color_R = 0;
+    int color_B = 0;
+    int color_G = 0;
 
     //---------------------------------------------------------------------------------------------
     public ColorPicker(Context context) {
@@ -54,9 +60,6 @@ public class ColorPicker extends AppCompatImageView
         mPaint.setStrokeWidth(sw);
         mPaint.setStyle(Paint.Style.STROKE);
 
-        //final int[] colors = new int[] { Color.CYAN, Color.MAGENTA, Color.YELLOW };
-        //final int[] colors = new int[] { Color.RED, Color.GREEN, Color.BLUE };
-
         final int[] colors = new int[] {
                 0xFFFF0000,
                 0xFFFFFF00,
@@ -83,17 +86,12 @@ public class ColorPicker extends AppCompatImageView
         mPaint.setShader(shader);
 
         RectF rectf = new RectF(center_x, center_y, width-center_x, height-center_y);
-        //c_bitmap.rotate(90, 300, 300);
         c_bitmap.drawCircle(center_x, center_y, radius - (sw / 2), mPaint);
-        //c_bitmap.drawArc(rectf, 20, 320, false, mPaint);
-
-        //mPaint.setStrokeWidth(2);
-        //c_bitmap.drawRect(0, 0, s, s, mPaint);
 
         mPaint.reset();
-        mPaint.setColor(Color.BLUE);
+        mPaint.setColor(Color.rgb(color_R, color_G, color_B));
         mPaint.setStyle(Paint.Style.FILL);
-        c_bitmap.drawCircle(center_x, center_y, width / 5, mPaint);
+        c_bitmap.drawCircle(center_x, center_y, width / 2 - sw - 20, mPaint);
 
         setImageBitmap(bitmap);
         setOnTouchListener(this);
@@ -102,21 +100,56 @@ public class ColorPicker extends AppCompatImageView
     //---------------------------------------------------------------------------------------------
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        ((MainActivity) getContext()).send_log(Color.BLACK, "x: " + String.valueOf(event.getX()));
-        ((MainActivity) getContext()).send_log(Color.BLACK, "y: " + String.valueOf(event.getY()));
 
         float x = event.getX();
         float y = event.getY();
 
+        //---
+        float correct_x = (getWidth() - bitmap.getWidth()) / 2;
+        x -= correct_x;
+        //---
+
+        if(x > bitmap.getWidth()) {
+            return false;
+        }
+        if(y > bitmap.getHeight()) {
+            return false;
+        }
+
         mPaint.reset();
         mPaint.setColor(bitmap.getPixel((int)x, (int)y));
         mPaint.setStyle(Paint.Style.FILL);
-        c_bitmap.drawCircle(center_x, center_y, width / 5, mPaint);
+        c_bitmap.drawCircle(center_x, center_y, width / 2 - sw - 20, mPaint);
 
         setImageBitmap(bitmap);
+        return true;
+    }
 
+    //---------------------------------------------------------------------------------------------
+    public void set_new_size(int new_width, int new_hight) {
+        width = new_width;
+        height = new_hight;
 
-        return false;
+        center_x = width / 2;
+        center_y = height / 2;
+        sw = width / 4;
+        radius = width / 2;
+
+        init();
+    }
+
+    //---------------------------------------------------------------------------------------------
+    public void set_color(int v_R, int v_G, int v_B) {
+        color_R = v_R;
+        color_G = v_G;
+        color_B = v_B;
+
+        mPaint.reset();
+        mPaint.setColor(Color.rgb(color_R, color_G, color_B));
+        mPaint.setStyle(Paint.Style.FILL);
+        c_bitmap.drawCircle(center_x, center_y, width / 2 - sw - 20, mPaint);
+
+        setImageBitmap(bitmap);
     }
 
     //---------------------------------------------------------------------------------------------
