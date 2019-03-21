3f7b78311680fe2b6100775df7975ba6185e05ce
RinatB2017 <tux4096@gmail.com>
2019-03-21 21:48:14 +0300

    update

--- _For_Testing/app/src/main/java/com/boss/for_testing/ColorPicker.java
+++ _For_Testing/app/src/main/java/com/boss/for_testing/ColorPicker.java
@@ -27,20 +27,24 @@ public class ColorPicker extends AppCompatImageView
     int center_y = height / 2;
     int radius = width / 2;
 
+    //---------------------------------------------------------------------------------------------
     public ColorPicker(Context context) {
         super(context);
     }
 
+    //---------------------------------------------------------------------------------------------
     public ColorPicker(Context context, AttributeSet attrs) {
         super(context, attrs, 0);
         init();
     }
 
+    //---------------------------------------------------------------------------------------------
     public ColorPicker(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init();
     }
 
+    //---------------------------------------------------------------------------------------------
     public void init() {
         bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 
@@ -50,12 +54,36 @@ public class ColorPicker extends AppCompatImageView
         mPaint.setStrokeWidth(sw);
         mPaint.setStyle(Paint.Style.STROKE);
 
-        final int[] colors = new int[] { Color.CYAN, Color.MAGENTA, Color.YELLOW };
+        //final int[] colors = new int[] { Color.CYAN, Color.MAGENTA, Color.YELLOW };
+        //final int[] colors = new int[] { Color.RED, Color.GREEN, Color.BLUE };
+
+        final int[] colors = new int[] {
+                0xFFFF0000,
+                0xFFFFFF00,
+                0xFF00FF00,
+                0xFF00FFFF,
+                0xFF0000FF,
+                0xFFFF00FF,
+                0xFFFF0000
+        };
+
+        /*
+        final int[] colors = new int[] {
+                0xFFFF0000,
+                0xFFFF00FF,
+                0xFF0000FF,
+                0xFF00FFFF,
+                0xFF00FF00,
+                0xFFFFFF00,
+                0xFFFF0000
+        };
+        */
+
         Shader shader = new SweepGradient(center_x, center_x, colors, null);
         mPaint.setShader(shader);
 
         RectF rectf = new RectF(center_x, center_y, width-center_x, height-center_y);
-        c_bitmap.rotate(90, 300, 300);
+        //c_bitmap.rotate(90, 300, 300);
         c_bitmap.drawCircle(center_x, center_y, radius - (sw / 2), mPaint);
         //c_bitmap.drawArc(rectf, 20, 320, false, mPaint);
 
@@ -71,6 +99,7 @@ public class ColorPicker extends AppCompatImageView
         setOnTouchListener(this);
     }
 
+    //---------------------------------------------------------------------------------------------
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         ((MainActivity) getContext()).send_log(Color.BLACK, "x: " + String.valueOf(event.getX()));
@@ -90,7 +119,10 @@ public class ColorPicker extends AppCompatImageView
         return false;
     }
 
+    //---------------------------------------------------------------------------------------------
     public int get_color() {
         return bitmap.getPixel(center_x, center_y);
     }
+
+    //---------------------------------------------------------------------------------------------
 }
--- _For_Testing/app/src/main/java/com/boss/for_testing/MagicCircle.java
+++ _For_Testing/app/src/main/java/com/boss/for_testing/MagicCircle.java
@@ -26,20 +26,33 @@ public class MagicCircle extends AppCompatImageView
     int center_x = width / 2;
     int center_y = height / 2;
 
+    int radius_1 = width / 2;
+    int radius_2 = width / 2 - 20;
+
+    float begin_angle = 20;
+    float end_angle   = 340;
+
+    float temp_x = 0;
+    float temp_y = 0;
+
+    //---------------------------------------------------------------------------------------------
     public MagicCircle(Context context) {
         super(context);
     }
 
+    //---------------------------------------------------------------------------------------------
     public MagicCircle(Context context, AttributeSet attrs) {
         super(context, attrs, 0);
         init();
     }
 
+    //---------------------------------------------------------------------------------------------
     public MagicCircle(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init();
     }
 
+    //---------------------------------------------------------------------------------------------
     public void init() {
         bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 
@@ -83,30 +96,104 @@ public class MagicCircle extends AppCompatImageView
         c_bitmap.drawArc(rectf1, 20, 320, false, mPaint);
         c_bitmap.drawArc(rectf2, 20, 320, false, mPaint);
 
+        //---
+        calc_line(center_x, center_y, 20, width / 2);
+        float begin_x1 = temp_x;
+        float begin_y1 = temp_y;
+
+        calc_line(center_x, center_y, 20, width / 2 - 20);
+        float end_x1 = temp_x;
+        float end_y1 = temp_y;
+
+        calc_line(center_x, center_y, 340, width / 2);
+        float begin_x2 = temp_x;
+        float begin_y2 = temp_y;
+
+        calc_line(center_x, center_y, 340, width / 2 - 20);
+        float end_x2 = temp_x;
+        float end_y2 = temp_y;
+
+        //mPaint.setColor(Color.GREEN);
+        c_bitmap.drawLine(begin_x1, begin_y1, end_x1, end_y1, mPaint);
+        //mPaint.setColor(Color.BLUE);
+        c_bitmap.drawLine(begin_x2, begin_y2, end_x2, end_y2, mPaint);
+        //---
+
         setImageBitmap(bitmap);
         setOnTouchListener(this);
     }
 
+    //---------------------------------------------------------------------------------------------
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        ((MainActivity) getContext()).send_log(Color.BLACK, "x: " + String.valueOf(event.getX()));
-        ((MainActivity) getContext()).send_log(Color.BLACK, "y: " + String.valueOf(event.getY()));
-
         float x = event.getX();
         float y = event.getY();
 
-        mPaint.reset();
-        mPaint.setColor(bitmap.getPixel((int)x, (int)y));
-        mPaint.setStyle(Paint.Style.FILL);
-        c_bitmap.drawCircle(center_x, center_y, width / 5, mPaint);
+        double dist = calc_dist(x, y);
+        if(dist > radius_1) {
+            return false;
+        }
+        if(dist < radius_2) {
+            return false;
+        }
+
+        float angle = calc_angle(x, y) - 90;
+        if(angle < 0) {
+            angle += 360.0f;
+        }
+
+        RectF rectf1 = new RectF(10,
+                10,
+                width - 10,
+                height - 10);
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(20);
+        //c_bitmap.rotate(90, center_x, center_y);
+        c_bitmap.drawArc(rectf1,
+                begin_angle,
+                angle - begin_angle,
+                false,
+                mPaint);
 
         setImageBitmap(bitmap);
-
-
-        return false;
+        return true;
     }
 
+    //---------------------------------------------------------------------------------------------
     public int get_color() {
         return bitmap.getPixel(center_x, center_y);
     }
+
+    //---------------------------------------------------------------------------------------------
+    private void calc_line(float x,
+                           float y,
+                           float angle,
+                           float radius) {
+        float A = radius;
+        float B = (float) Math.cos(Math.toRadians(angle)) * A;
+        float C = (float) Math.sin(Math.toRadians(angle)) * A;
+
+        temp_x = x + B;
+        temp_y = y + C;
+    }
+
+    //---------------------------------------------------------------------------------------------
+    private double calc_dist(float x,
+                             float y) {
+        double dist_x = Math.abs(x - center_x);
+        double dist_y = Math.abs(y - center_y);
+
+        double dist = Math.sqrt(dist_x * dist_x + dist_y * dist_y);
+        return dist;
+    }
+
+    //---------------------------------------------------------------------------------------------
+    private float calc_angle(float x,
+                              float y) {
+        double radians = Math.atan2(y - center_y, x - center_x);
+        float angle = (float)radians * 180.0f / (float)Math.PI;
+        return angle;
+    }
+
+    //---------------------------------------------------------------------------------------------
 }
