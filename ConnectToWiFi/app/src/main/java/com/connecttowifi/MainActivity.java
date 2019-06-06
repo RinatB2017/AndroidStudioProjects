f65a48669b28d6490a03398957c9ca9eedbb177a
RinatB2017 <tux4096@gmail.com>
2019-06-06 19:33:38 +0300

    update

--- ConnectToWiFi/app/src/main/java/com/connecttowifi/MainActivity.java
+++ ConnectToWiFi/app/src/main/java/com/connecttowifi/MainActivity.java
@@ -11,14 +11,14 @@ import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiConfiguration.KeyMgmt;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
 	private WifiManager wifiManager;
 	private WifiConfiguration wifiConfig;
