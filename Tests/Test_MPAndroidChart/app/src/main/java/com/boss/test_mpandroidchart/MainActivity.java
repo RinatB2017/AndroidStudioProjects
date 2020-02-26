49ca5d6a87d89c2c3cebaa30e435aae0f3942800
RinatB2017 <tux4096@gmail.com>
2020-02-26 16:57:23 +0300

    update

---
+++ Tests/Test_MPAndroidChart/app/src/main/java/com/boss/test_mpandroidchart/MainActivity.java
@@ -0,0 +1,61 @@
+package com.boss.test_mpandroidchart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LineChart chart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chart = findViewById(R.id.chart1);
+    }
+
+    public void test(View view) {
+        ArrayList<Entry> values = new ArrayList<>();
+
+        // increment by 1 hour
+        for (float x = 0; x < 100; x++)
+        {
+            values.add(new Entry(x, x)); // add one entry per hour
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(values, "DataSet 1");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setValueTextColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(1.5f);
+        set1.setDrawCircles(false);
+        set1.setDrawValues(false);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        set1.setDrawCircleHole(false);
+
+        // create a data object with the data sets
+        LineData data = new LineData(set1);
+        data.setValueTextColor(Color.WHITE);
+        data.setValueTextSize(9f);
+
+        // set data
+        chart.setData(data);
+        chart.invalidate();
+    }
+}
