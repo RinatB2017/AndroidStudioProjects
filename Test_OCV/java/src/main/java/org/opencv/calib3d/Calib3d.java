bd563abe07888f9b713e5fc8a138082727056a1c
tux4096@gmail.com <tux4096@gmail.com>
2021-02-05 15:10:32 +0300

    update

--- Test_OCV/java/src/main/java/org/opencv/calib3d/Calib3d.java
+++ Test_OCV/java/src/main/java/org/opencv/calib3d/Calib3d.java
@@ -3,9 +3,6 @@
 //
 package org.opencv.calib3d;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.opencv.calib3d.UsacParams;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDouble;
 import org.opencv.core.MatOfPoint2f;
@@ -16,6 +13,9 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.core.TermCriteria;
 import org.opencv.utils.Converters;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // C++: class Calib3d
 
